370ac57335b1803c994b56d2dbb7c0e7c474ac04
André Felipe Chaves Nerva <andrenerva@MacBook-Air-de-Andre.local>
2017-05-09 23:43:01 -0300

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('bioimpedancia.controllers', [])
+
+.controller('CadastroController', function($scope, $state) {
+
+  $scope.goToResultado = function() {
+    $state.go('tab.resultado');
+  }
+
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('SobreCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
