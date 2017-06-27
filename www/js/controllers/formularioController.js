9bb5cf5d8c887a04a29f11759b170533ab6b18a5
André Felipe Chaves Nerva <andrenerva@MacBook-Air-de-Andre.local>
2017-06-27 02:31:29 -0300

    Gerando resultados.

--- www/js/controllers/formularioController.js
+++ www/js/controllers/formularioController.js
@@ -12,7 +12,12 @@ angular.module('bioimpedancia.formularioController', [])
   }
 
   $scope.goToCadastrar = function() {
-    calculo();
+    if($scope.sexo.id == 0) {
+      calculoFem();
+    } else if($scope.sexo.id == 1) {
+      calculoMasc();
+    } 
+    
     $state.go('tab.resultado');
   }
 
@@ -20,13 +25,13 @@ angular.module('bioimpedancia.formularioController', [])
     console.log($scope.sexo);
   }
 
-  function calculo() {
+  function calculoMasc() {
     
     console.log($scope.informacoes)
 
-    var resistencia = 1;
+    var resistencia = 578;
     var bodyDensity = 1.1554 - (0.0841 * $scope.informacoes.peso * (resistencia/($scope.informacoes.altura * $scope.informacoes.altura)));
-    var fatMass = ((4.95/(bodyDensity - 4.5))) * 100;
+    var fatMass = ((4.95/bodyDensity - 4.5)) * 100;
     var ffm = $scope.informacoes.peso - fatMass;
 
     resultadoService.dataObj.bodyDensity = bodyDensity;
@@ -39,6 +44,23 @@ angular.module('bioimpedancia.formularioController', [])
 
   }
   
-  
+    function calculoFem() {
+    
+    console.log($scope.informacoes)
+
+    var resistencia = 578;
+    var bodyDensity = 1.1554 - (0.0841 * $scope.informacoes.peso * (resistencia/($scope.informacoes.altura * $scope.informacoes.altura)));
+    var fatMass = (1 - [0.3981 * (($scope.informacoes.altura * $scope.informacoes.altura)/resistencia) + 0.3066 * $scope.informacoes.peso + 0.095299 * ( $scope.informacoes.peso - 100 ) + 0.7414] / $scope.informacoes.peso) * 100;
+    var ffm = $scope.informacoes.peso - fatMass;
+
+    resultadoService.dataObj.bodyDensity = bodyDensity;
+    resultadoService.dataObj.fatMass = fatMass;
+    resultadoService.dataObj.ffm = ffm;
+
+    console.log(bodyDensity);
+    console.log(fatMass);
+    console.log(ffm);
+
+  }
 
 })
--- www/js/controllers/resultadoController.js
+++ www/js/controllers/resultadoController.js
@@ -2,6 +2,12 @@ angular.module('bioimpedancia.resultadoController', [])
 
 .controller('ResultadoController', function($scope, $state, $location, resultadoService) {
 
-    $scope.resultado = 'resultado = ' + resultadoService.dataObj.bodyDensity
+    $scope.$on('$ionicView.loaded', function () {
+      //Number(resultadoService.dataObj.bodyDensity.replace(/[^0-9\.]+/g,""))
+    });
+
+    $scope.bodyDensity = "" + resultadoService.dataObj.bodyDensity;
+    $scope.fatMass = " " + resultadoService.dataObj.fatMass;
+    $scope.ffm = " " + resultadoService.dataObj.ffm;
 
 })    
