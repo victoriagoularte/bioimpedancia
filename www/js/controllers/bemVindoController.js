5a067cb883fee2638bbdd48414e1457a67ac010f
Letícia Ribeiro Miranda <leticiaribeiro@aluno.unb.br>
2017-06-29 00:02:38 -0300

    Getting service from services list

--- www/js/controllers/bemVindoController.js
+++ www/js/controllers/bemVindoController.js
@@ -5,6 +5,7 @@ angular.module('bioimpedancia.bemVindoController', [])
     const URI = 'http://homol.redes.unb.br/uiot-raise/';
     const CLIENT_REGISTER = 'client/register/';
     const SERVICE_LIST = 'service/list/';
+    const DATA_LIST = 'data/list/';
 
     $scope.tokenId = "";
 
@@ -22,23 +23,22 @@ angular.module('bioimpedancia.bemVindoController', [])
 
     $scope.raiseClientRegister = function () {
       $http.post(URI + CLIENT_REGISTER, {
-          "name": "BIA-App",
-          "chipset": "Apple A10 Fusion",
-          "mac": "00:db:df:07:6d:13",
-          "serial": "X0XX0X00X0X0",
-          "processor": "Arm A10",
-          "channel": "Wi-Fi",
-          "client_time": 1315584200,
-          "tag": [
+        "name": "BIA-App",
+        "chipset": "Apple A10 Fusion",
+        "mac": "00:db:df:07:6d:13",
+        "serial": "X0XX0X00X0X0",
+        "processor": "Arm A10",
+        "channel": "Wi-Fi",
+        "client_time": 1315584200,
+        "tag": [
 
-          ]
-        }
-      ).success(function mySuccess(response) {
+        ]
+      }).success(function mySuccess(response) {
         $scope.raiseResponse = response.message;
         console.log($scope.raiseResponse);
         $scope.tokenId = response.tokenId;
         $scope.raiseServiceList();
-      }).error( function myError(data, status) {
+      }).error(function myError(data, status) {
         $scope.raiseResponse = data;
         console.log($scope.raiseResponse);
       });
@@ -46,9 +46,22 @@ angular.module('bioimpedancia.bemVindoController', [])
 
     $scope.raiseServiceList = function () {
       // param service_name not working
+      $scope.list = [];
       $http.get(URI + SERVICE_LIST + '?tokenId=' + $scope.tokenId)
-        .success(function (response){
+        .success(function (response) {
           $scope.raiseResponse = response;
+          angular.forEach($scope.raiseResponse.values, function (elem, index) {
+            var services = [];
+            var getService;
+            if (elem.services != null) {
+              services = elem.services;
+              getService = services.filter((service) => service.name === "GValues_M")[0];
+              if(getService != null){
+                $scope.list.push(getService);
+              }
+            }
+          });
+          $scope.service = $scope.list.sort(function (a, b) { return b.service_id - a.service_id; })[0];
         });
     };
 
